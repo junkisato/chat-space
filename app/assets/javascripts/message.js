5a2c4a6bfe63ae04a551162f6dd7164fbe9b9c79
DING <1206438158@qq.com>
2019-07-18 16:15:40 +0900

    updating_ok

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -48,4 +48,77 @@ $(function(){
       alert('error');
     })
   })
+  var buildMessageHTML = function(message) {
+
+      var user_date =`<div class="message__upper-info">
+        <div class="message__upper-info__talker">
+          ${message.user_name}
+        </div>
+        <div class="message__upper-info__date">
+          ${message.created_at}
+        </div>
+      </div>`
+
+      var up_text =`<p class="message__text">
+        ${message.content}
+      </p>`
+
+      var up_image =`<img src=${message.image.url} class="message__image" >`
+
+    if (message.content && message.image.url) {
+      //data-idが反映されるようにしている
+      var html = `<div class="message" data-id= ${message.id}>
+        ${user_date}
+        ${up_text}
+        ${up_image}
+      </div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-id= ${message.id}>
+        ${user_date}
+        ${up_text}
+      </div>`
+    } else if (message.image.url) {
+      //同様に、data-idが反映されるようにしている
+      var html = `<div class="message" data-id= ${message.id}>
+        ${user_date}
+        ${up_image}
+      </div>`
+    };
+    return html;
+  };
+
+  var reloadMessages = function() {
+    last_message_id = $('.message').last().data('id');
+    group_id = $('.main-header__left-box__current-group').data('id')
+    var url =`/groups/${group_id}/api/messages`
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json', 
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message){
+        //メッセージが入ったHTMLを取得
+        var html = buildMessageHTML(message)
+        //メッセージを追加
+        insertHTML += html
+        console.log(insertHTML);
+      })
+      if(insertHTML !== ''){
+      $('.messages').append(insertHTML).animate({scrollTop:$('.messages')[0].scrollHeight}, 1500, 'swing');
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+   var now_url = location.pathname ;
+   group_id = $('.main-header__left-box__current-group').data('id')
+   var OKurl =`/groups/${group_id}/messages`
+  if(now_url == OKurl){
+  setInterval(reloadMessages, 5000);
+  }
 })
