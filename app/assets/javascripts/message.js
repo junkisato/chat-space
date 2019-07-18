a5fea675f16450de7ab4fad1577af2616b425718
DING <1206438158@qq.com>
2019-07-18 17:19:16 +0900

    fix1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,16 @@
 $(function(){
   function buildHTML(message){
     
-    if(message.image.url !== null){
-      var content =`<img class="message__image" src=${message.image.url}>`
-    }
-    else{
-      var content =""
-    }
+    // if(message.image.url !== null){
+    //   var content =`<img class="message__image" src=${message.image.url}>`
+    // }
+    // else{
+    //   var content =""
+    // }
     // 一行で書くこともできる
-    // var content = message.image.url ? `<img class="message__image" src=${message.image.url}>` : "";
+    var image = message.image.url ? `<img class="message__image" src=${message.image.url}>` : "";
     
-    var html = `<div class="message">
+    var html = `<div class="message" data-id= ${message.id}>
                   <div class="message__upper-info">
                     <div class="message__upper-info__talker">
                       ${message.name}
@@ -22,7 +22,7 @@ $(function(){
                   <p class="message__text">
                     ${message.content}
                   </p>
-                  ${content}
+                  ${image}
                 </div>`
     return html;
   }
@@ -40,52 +40,16 @@ $(function(){
     })
     .done(function(message){
       var html = buildHTML(message);
-      $('.messages').append(html);
+      $('.messages').append(html).animate({scrollTop:$('.messages')[0].scrollHeight}, 1500, 'swing');
       $('#new_message')[0].reset();
       $(".submit-btn").prop("disabled", false);
+
     })
     .fail(function(){
       alert('error');
+      $(".submit-btn").prop("disabled", false);
     })
   })
-  var buildMessageHTML = function(message) {
-
-      var user_date =`<div class="message__upper-info">
-        <div class="message__upper-info__talker">
-          ${message.user_name}
-        </div>
-        <div class="message__upper-info__date">
-          ${message.created_at}
-        </div>
-      </div>`
-
-      var up_text =`<p class="message__text">
-        ${message.content}
-      </p>`
-
-      var up_image =`<img src=${message.image.url} class="message__image" >`
-
-    if (message.content && message.image.url) {
-      //data-idが反映されるようにしている
-      var html = `<div class="message" data-id= ${message.id}>
-        ${user_date}
-        ${up_text}
-        ${up_image}
-      </div>`
-    } else if (message.content) {
-      var html = `<div class="message" data-id= ${message.id}>
-        ${user_date}
-        ${up_text}
-      </div>`
-    } else if (message.image.url) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-id= ${message.id}>
-        ${user_date}
-        ${up_image}
-      </div>`
-    };
-    return html;
-  };
 
   var reloadMessages = function() {
     last_message_id = $('.message').last().data('id');
@@ -102,10 +66,9 @@ $(function(){
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message){
         //メッセージが入ったHTMLを取得
-        var html = buildMessageHTML(message)
+        var html = buildHTML(message)
         //メッセージを追加
         insertHTML += html
-        console.log(insertHTML);
       })
       if(insertHTML !== ''){
       $('.messages').append(insertHTML).animate({scrollTop:$('.messages')[0].scrollHeight}, 1500, 'swing');
