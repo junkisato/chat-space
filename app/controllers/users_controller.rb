925c291f5faa829463d089557aa88e5f115ef18e
DING <1206438158@qq.com>
2019-07-20 12:58:49 +0900

    fix1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   
-  def index
-    @users = User.where("name LIKE(?) AND id != ?", "%#{params[:keyword]}%",current_user)
+  def index 
+    @users =User.where("name LIKE(?) AND id != ?", "%#{params[:keyword]}%",current_user)
+    # users =user.where('id != ?', params[:input_user])
+    # if users.respond_to?
+    #   @users.push(users)
+    #   @users.flatten!
+    # end
     respond_to do |format|
       format.html
       format.json
